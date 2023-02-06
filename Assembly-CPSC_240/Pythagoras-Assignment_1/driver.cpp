89e6694e3fb328748e852a7ec9b02fe43a956fb5
tonyzalez <antoniog@csu.fullerton.edu>
2023-02-05 21:32:52 -0800

    Completed

--- Assembly-CPSC_240/Pythagoras-Assignment_1/driver.cpp
+++ Assembly-CPSC_240/Pythagoras-Assignment_1/driver.cpp
@@ -2,10 +2,10 @@
 //Program name: "Pythagoras".  This program takes in the user input of two sides of a right triangle in float and calculates 
 //the hypotenuse. Copyright (C) 2023 Antonio Gonzalez.                                                                       *
 //                                                                                                                           *
-//This file is part of the software program "Pythagoras".                                                                     *
-//Pythagoras is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License     *
+//This file is part of the software program "Pythagoras".                                                                    *
+//Pythagoras is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License    *
 //version 3 as published by the Free Software Foundation.                                                                    *
-//Pythagoras is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied            *
+//Pythagoras is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied           *
 //warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
 //A copy of the GNU General Public License v3 is available here:  <https:;www.gnu.org/licenses/>.                            *
 //****************************************************************************************************************************
@@ -20,8 +20,8 @@
 //  Program name: Pythagoras
 //  Programming languages: One modules in C++ and one module in X86
 //  Date program began: 2023 Jan 23
-//  Date of last update: 2023 Feb 3
-//  Files in this program: driver.cpp, pythagoras.asm
+//  Date of last update: 2023 Feb 4
+//  Files in this program: driver.cpp, pythagoras.asm, run.sh
 //  Status: Finished.
 //
 //Purpose
@@ -31,32 +31,32 @@
 //   File name: driver.cpp
 //   Language: C++
 //   Max page width: 132 columns
-//   Compile: gcc -c -Wall -m64 -no-pie -o manage-floats.o manage-floats.c -std=c17
-//   Link: gcc -m64 -no-pie -o three-numbers.out manage-floats.o float-input-output.o -std=c17
-//   Optimal print specification: 132 columns width, 7 points, monospace, 8½x11 paper
+//   Compile: g++ -c -Wall -no-pie -m64 -std=c++17 -o driver.o driver.cpp
+//   Link: g++ -m64  -no-pie -o pyth.out pythagoras.o driver.o -std=c++17
 //
 //=======1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
 //
 //
 //===== Begin code area ===========================================================================================================
 
-#include <stdio.h>
-#include <stdint.h>
-#include <time.h>
-#include <sys/time.h>
-#include <stdlib.h>
+
 #include <iostream>
+#include <cstdio>
+#include <iomanip>
 
 extern "C" double pythagoras();
 
 int main(int argc, char *argv[]){
 
     std::cout << "Welcome to Pythagoras Math Lab programmed by Antonio Gonzalez\n"
-              <<"Please contact me at antoniog@csu.fullerton.edu if you need assistance" << std::endl;
+              <<"Please contact me at antoniog@csu.fullerton.edu if you need assistance\n" << std::endl;
     
     double hypotenuse = pythagoras();
 
-    std::cout << "The main file received this number: " << hypotenuse<< ", and will keep it for now.\n"
+    std::cout << "The main file received this number: " << std::setprecision(12) << hypotenuse
+              << ", and will keep it for now.\n"
               << "We hoped you enjoyed your right angles. Have a good day. A zero will be sent to your operating system.\n";
+
+    return 0;
 }
             
--- Assembly-CPSC_240/Pythagoras-Assignment_1/pythagoras.asm
+++ Assembly-CPSC_240/Pythagoras-Assignment_1/pythagoras.asm
@@ -1,6 +1,46 @@
+;***************************************************************************************************************************
+;Program name: "Pythagoras".  This program calculates the the hypotenuse of a right triangle in x86                         *
+;Copyright (C) 2023 Antonio Gonzalez                                                                                        *
+;This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
+;version 3 as published by the Free Software Foundation.                                                                    *
+;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         *
+;warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
+;A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.                           *
+;****************************************************************************************************************************
 
 
-;declare constants outside functions this fucntion
+
+;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3=========4=========5=========6=========7**
+
+;Author information
+;  Author name: Antonio Gonzalez
+;  Author email: antoniog@fullerton.edu
+;
+;Program information
+;  Program name: Pythagoras
+;  Programming languages: One modules in C++ and one module in X86
+;  Date program began: 2023 Jan 23
+;  Date of last update: 2023 Feb 4
+;  Comments reorganized: 2023 Feb 4
+;  Files in the program: driver.cpp, pythagoras.asm, run.sh
+;
+;Purpose
+;  Calculate the hypotenuse of a right trinagle in x86
+;
+;This file
+;  File name: pythagoras.asm
+;  Language: X86-64
+;  Syntax: Intel
+;  Max page width: 172 columns
+;  Optimal print specification: Landscape, 7-point font, monospace, 172 columns, 8½x11 paper
+;  Assemble: nasm -f elf64 -l pythagoras.lis -o pythagoras.o pythagoras.asm
+;
+;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3=========4=========5=========6=========7**
+;
+;
+;===== Begin code area ====================================================================================================================================================
+
+
 extern printf
 extern scanf
 global pythagoras:
@@ -8,9 +48,10 @@ global pythagoras:
 segment .data
 length1 db "Enter the length of the first side of the triangle: ",0
 length2 db "Enter the length of the second side of the triangle: ",0
-response db "Thank you. You entered two sides: %1.4lf and %1.4lf",10 ,0
-hypotenuse db "The length of the hypotenuse is %1.4lf",10, 0
+response db "Thank you. You entered two sides: %1.6lf and %1.6lf",10 ,0
+hypotenuse db "The length of the hypotenuse is %1.6lf",10 ,10 ,0
 double_form db "%lf", 0 
+newline db 10
 
 segment .bss
 ;Empty
@@ -38,22 +79,22 @@ push rbx                                                    ;Backup rbx
 pushf                                                       ;Backup rflags
 
 
-;Prompt for side 1
-push qword 0
+push qword 0                    ;create space on stack
 
+;Prompt for side 1
 push qword 0
 mov rax, 0 
 mov rdi, length1
-call printf
+call printf                     ;printf takes argument from rdi
 pop rax
 
-;Get side 1 and input to xmm12
+;First input to xmm12
 push qword 0                    ;push 8 bytes of zeroes onto stack 
 mov rax, 0
 mov rdi, double_form
 mov rsi, rsp                    ;function does not recognize rsp, only rsi as second argument
-call scanf
-movsd xmm12, [rsp]
+call scanf                      ;looks at top of stack hence previous instruction 
+movsd xmm12, [rsp]              ;[rsp] dereferences rsp
 pop rax
 
 ;Prompt for side 2
@@ -63,27 +104,34 @@ mov rdi, length2
 call printf
 pop rax
 
-;Second float to xmm13
+;Second input to xmm13
 push qword 0   
 mov rax, 0
 mov rdi, double_form            
 mov rsi, rsp             
 call scanf
-movsd xmm13, [rsp]
+movsd xmm13, [rsp]              ;[rsp] dereferences rsp
 pop rax
 
-;Print 2 sides
-push qword 0
-mov rax, 2 
-mov rdi, response 
-movsd xmm0, xmm12
-movsd xmm1, xmm13
+;Newline to separate input and output section 
+push qword 0 
+mov rax, 0 
+mov rdi, newline
 call printf
+pop rax
+
+;Print the two sides
+push qword 0
+mov rax, 2                      ;Handling 2 xmm registers this block, hence the 2
+mov rdi, response 
+movsd xmm0, xmm12               ;movsd for xmm register 
+movsd xmm1, xmm13
+call printf                     ;printf takes lower xmm registers as arguments
 pop rax
 
 ;square side 1 and 2
 mov rax, 2
-mulsd xmm12, xmm12
+mulsd xmm12, xmm12 
 mulsd xmm13, xmm13
 
 ;add side 1 and 2 
@@ -91,21 +139,20 @@ mov rax, 1
 addsd xmm12, xmm13
 
 ;compute hypotenuse
-mov rax, 2 
+mov rax, 1
 sqrtsd xmm12, xmm12
-movsd xmm13, xmm12
 
 ;print out hypotenuse
 push qword 0 
 mov rax, 1
 mov rdi, hypotenuse
-movsd xmm0, xmm13
+movsd xmm0, xmm12               ;move hypotenuse to xmm0 for printf as argument
 call printf
 pop rax
 
 ;return to driver 
 mov rax, 1
-movsd xmm0, xmm13
+movsd xmm0, xmm12               ;when ret is called, function returns xmm0 to driver
 
 pop rax
 
