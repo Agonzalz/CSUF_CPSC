3d81c8de13b1002b325bce65beec06c00411d827
tonyzalez <antoniog@csu.fullerton.edu>
2023-05-14 21:27:55 -0700

    changed
    changed

--- Assembly-CPSC_240/assignment_6/r.sh
+++ Assembly-CPSC_240/assignment_6/r.sh
@@ -12,10 +12,10 @@ echo "Assemble birthday.asm"
 nasm -f elf64 -l birthday.lis -o birthday.o birthday.asm
 
 echo "compile main.cpp using g++ compiler standard 2020"
-g++ -c -m64 -Wall -std=c++20 -no-pie -o main.o main.cpp
+g++ -c -m64 -Wall -std=c++2a -no-pie -o main.o main.cpp
 
 echo "Link object files using the g++ Linker standard 2020"
-g++ -m64 -std=c++20 -no-pie -o final-sleep-time.out main.o birthday.o
+g++ -m64 -std=c++2a -no-pie -o final-sleep-time.out main.o birthday.o
 
 echo "Run the Sleep Time Program:"
 ./final-sleep-time.out
