252468378d2616be77bb3539fae3ae1e501cba7e
lelaxis <antoniog@csu.fullerton.edu>
2023-09-17 15:14:02 -0700

    added_assignment

---
+++ Operating_systems/assignments/grand_child.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main() {
+    pid_t id;
+    pid_t child_id;
+
+    id = fork();
+    if (id < 0) {
+		fprintf(stderr, "Fork Failed");
+		exit(-1);
+    }
+    if (id == 0) {
+        fprintf(stderr, "This is child process %d \n", getpid());
+        child_id = fork();
+        if (child_id < 0) {
+		    fprintf(stderr, "Fork Failed");
+		    exit(-1);
+         }
+        if (child_id == 0) {
+            execlp("firefox","firefox", NULL);
+        } else {
+            printf("child is waiting\n"); 
+            wait(NULL);
+        }
+    } else {
+        fprintf(stderr, "This is parent process %d \n", getpid());
+        printf("parent is waiting\n");
+        wait (NULL);
+    }
+    return 0;
+}
