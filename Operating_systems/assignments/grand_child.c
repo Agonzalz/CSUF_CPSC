30d559e5c3376ad30a3b2f08e2d6b4023137e197
Agonzalz <asgpersona@gmail.com>
2023-09-17 22:07:07 -0700

    wait check shorter

--- Operating_systems/assignments/grand_child.c
+++ Operating_systems/assignments/grand_child.c
@@ -8,28 +8,32 @@ int main() {
     pid_t id;
     pid_t child_id;
 
+    //parent and child fork starts here
     id = fork();
+    //error handling
     if (id < 0) {
 		fprintf(stderr, "Fork Failed");
 		exit(-1);
     }
-    if (id == 0) {
+    if (id > 0) {
+        fprintf(stderr, "This is parent process %d \n", getpid());
+    } else {
         fprintf(stderr, "This is child process %d \n", getpid());
+        
+        //child and grandchild fork starts here
         child_id = fork();
+        //error handling
         if (child_id < 0) {
 		    fprintf(stderr, "Fork Failed");
 		    exit(-1);
          }
         if (child_id == 0) {
-            execlp("/snap/bin/firefox","firefox", NULL);
-        } else {
-            printf("child is waiting\n"); 
-            wait(NULL);
+            fprintf(stderr, "This is grandchild process %d \n", getpid());
+            fprintf(stderr, "grandchild will open firefox\n");
+            execlp("firefox","firefox", NULL);
         }
-    } else {
-        fprintf(stderr, "This is parent process %d \n", getpid());
-        printf("parent is waiting\n");
-        wait (NULL);
     }
+    fprintf(stderr, "process %d is waiting\n", getpid());
+    wait(NULL);
     return 0;
 }
