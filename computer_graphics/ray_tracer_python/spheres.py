c79101d4d3087b5d6f676626f92f20b2f0f710d5
lelaxis <antoniog@csu.fullerton.edu>
2024-02-07 12:47:46 -0800

    modified

--- computer_graphics/ray_tracer_python/spheres.py
+++ computer_graphics/ray_tracer_python/spheres.py
@@ -34,23 +34,6 @@ class Spheres:
         objects.extend(Spheres.create_cluster(-2, 2, -6, 0, 0, 0.8))
         objects.extend(Spheres.create_cluster(-1, 3, -2, 0.7, 0, 0.6))
         objects.extend(Spheres.create_cluster(1, 3,-7, 0.4, 0.6, 0.5))
-        # i = 1 
-        # x = -2
-        # y = 0 
-        # z = -1 
-        # c = 0.1
-        # while i < 8 : 
-        #     objects.append(Sphere(center=(1 - c, 0, z), radius=0.3, diffuse=(0.7, c, 0)))
-        #     objects.append(Sphere(center=(1, y, -4), radius=0.3, diffuse=(c, 0.7, 0)))
-        #     objects.append(Sphere(center=(x, 0, -4), radius=0.3, diffuse=(0.7, 0, c)))
-        #     # objects.append(Sphere(center=(a, 3, -1), radius=0.3, diffuse=(b, 0, 0.7)))
-        #     # objects.append(Sphere(center=(a, 4, -1), radius=0.3, diffuse=(0, b, 0.7)))
-        #     x += 1
-        #     y += 1
-        #     c += 0.1 
-        #     z -= 1
-        #     i += 1 
-
         
         return objects
 
@@ -61,4 +44,4 @@ class Spheres:
 
     @staticmethod
     def get_camera():
-        return np.array([0, 0.2, 1])
+        return np.array([0, 0, 1])
